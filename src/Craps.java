6b56f282a22e88c0a37fb6e2b6010b4c1c5ae437
JimmyJangler <roarknoah2@gmail.com>
2024-03-03 11:36:13 -0500

    Project INIT

---
+++ src/Craps.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Craps
+{
+    public static void main(String[] args)
+    {
+        //variable declarations + random util + scanner util
+        Random rnd = new Random();
+
+    }
+}
